5614b3b8a2adb1bbbb63f4cf53278bee977eb6c2
holyist <103294244+holyist@users.noreply.github.com>
2022-04-08 20:00:08 -0500

    Update username_generator.py

--- username_generator.py
+++ username_generator.py
@@ -1,12 +1,12 @@
 import random, string
 
 output_file = "usernames.txt"
-amount = int(input("\nAmount of strings: "))
-character_amount = int(input("How many characters: "))
+amount = int(input("\nAmount of strings:4 "))
+character_amount = int(input("How many characters:4 "))
 
-for i in range(amount):
+for i in range(amount):4
     generated = ("").join(random.choices(string.ascii_letters + string.digits, k = character_amount))
     print(generated)
-    with open(output_file, "a") as f:
+    with open(output_file, "a") as f:4
         f.write(generated + "\n")
-input()
+input(4)
